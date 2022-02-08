797227819b941556abd998304bf3bfa5e874cb17
Darren Finch <finchdarren25@gmail.com>
2022-02-07 21:54:18 -0600

    Partial coding problem solution 2

--- main.kt
+++ main.kt
@@ -14,3 +14,40 @@ fun twoSum(nums: IntArray, target: Int): IntArray {
     
     return intArrayOf(); //wont be reached
 }
+
+
+
+// HALF SOLVED - TODO
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+        // In classic addition, from right to left: 
+        // 1. Add everything in first column
+        // 2. Add ones digit of that addition to final answer.
+        // 3. Add tens place (if it exists) to carry val
+        // 4. In next column, do the addition again, and this time, add the carry val
+        // 5. Repeat until you finish adding sum of last column to final answer
+        //
+        // In a linked-list environment, you will have pointers to the elements you're trying to work with
+        var pointer1 = l1
+        var pointer2 = l2
+        var ans = ListNode(0)
+        
+        while (pointer1 != null || pointer2 != null) {
+            val sumResult = pointer1.`val` + pointer2.`val`
+            val digitToAddToAns = sumResult % 10
+            val amtToCarry = ((sumResult / 10) as String).split(".")[0]
+            print(digitToAddedToAns)
+            print(git)
+        }
+        
+    }
+}
