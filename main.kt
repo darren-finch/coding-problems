9d5f386040a4deeebcbebf432b16725aab217e10
Darren Finch <finchdarren25@gmail.com>
2022-02-24 09:16:06 -0600

    New broken binary search

--- main.kt
+++ main.kt
@@ -267,3 +267,28 @@ class Solution {
         return k
     }
 }
+
+
+
+// Broken binary search... again
+/* The isBadVersion API is defined in the parent class VersionControl.
+      fun isBadVersion(version: Int) : Boolean {} */
+
+      class Solution: VersionControl() {
+        override fun firstBadVersion(n: Int) : Int {
+            var left = 1
+            var right = n
+            var mid = (left + right) / 2
+            
+            while (left < right) {
+                mid = (left + right) / 2
+                if (isBadVersion(mid)) {
+                    right = mid - 1
+                } else {
+                    left = mid + 1
+                }
+            }
+            
+            return mid
+        }
+    }
