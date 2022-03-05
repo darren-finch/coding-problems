ffc1d934e63d00ccc989bc33b052d0e1c5d97bd8
Darren Finch <finchdarren25@gmail.com>
2022-03-05 15:47:18 -0600

    Finally finished search insert position

--- main.kt
+++ main.kt
@@ -405,3 +405,29 @@ class Solution {
         }
     }
 }
+
+
+
+// SOLVED (with a little help)
+class Solution {
+    fun searchInsert(nums: IntArray, target: Int): Int {
+        var l = 0
+        var r = nums.size - 1
+        
+        while (l <= r) {
+            var mid = l + ((r - l) / 2)
+            
+            if (nums[mid] == target) {
+                return mid
+            }
+            
+            if (nums[mid] < target) {
+                l = mid + 1
+            } else {
+                r = mid - 1
+            }
+        }
+        
+        return l
+    }
+}
