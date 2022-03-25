0a67c003d2e29dcda6d0cf03e9e7ffa484d2ea64
Darren Finch <finchdarren25@gmail.com>
2022-03-24 22:37:25 -0500

    Reverse string

--- main.kt
+++ main.kt
@@ -650,3 +650,19 @@ class Solution {
 }
 
 data class GraphNode(val value: Int, val i: Int, val j: Int, val distanceFromStart: Int)
+
+
+
+class Solution {
+    fun reverseString(s: CharArray): Unit {
+        var left = 0
+        var right = s.size - 1
+        while (left < right) {
+            val temp = s[left]
+            s[left] = s[right]
+            s[right] = temp
+            left++
+            right--
+        }
+    }
+}
