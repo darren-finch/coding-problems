bd97649e3714886daac897477bb67749e6f0ac93
Darren Finch <finchdarren25@gmail.com>
2022-05-02 22:56:06 -0500

    Conditionals

--- main.js
+++ main.js
@@ -12,3 +12,14 @@ function doubleChar(str) {
 function betterThanAverage(classPoints, yourPoints) {
 	return yourPoints > classPoints.reduce((a, b) => a + b, 0) / classPoints.length; 
   }
+
+const rps = (p1, p2) => {
+	if(p1 == p2)
+		return "Draw!";
+	else {
+		if((p1=="rock"&&p2=="scissors")||(p1=="scissors"&&p2=="paper")||(p1=="paper"&&p2=="rock"))
+			return "Player 1 won!";
+		else 
+			return "Player 2 won!";
+	}
+};
