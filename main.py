c4272479a175dc46883b12e9f8bdf59514903da0
Darren Finch <finchdarren25@gmail.com>
2022-05-13 21:58:09 -0500

    Permutations solution, still understanding

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Permutations solution, had to look at it for this one. Still trying to understand.
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         result = []
@@ -6,8 +7,7 @@ class Solution:
             return [nums[:]]
         
         for i in range(len(nums)):
-            n = nums.pop(i)
-            print(nums)
+            n = nums.pop(0)
             perms = self.permute(nums)
             for perm in perms:
                 perm.append(n)
@@ -16,4 +16,3 @@ class Solution:
             result.extend(perms)
             
         return result
-        
