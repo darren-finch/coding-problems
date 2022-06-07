253260cfd6aece1d7384c50c266d3d0b2e6465be
Darren Finch <finchdarren25@gmail.com>
2022-06-06 22:26:46 -0500

    Search Insert WIP

--- main.py
+++ main.py
@@ -35,3 +35,23 @@ class Solution:
             p2 += 1
                 
         return k
+
+
+
+// WIP
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums)
+        
+        while l < r:
+            mid = int((l + r) / 2)
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            elif target < nums[mid]:
+                r = mid - 1
+        
+        return l
