7750743b59553e04e14157a262ebce9c7ef496d8
Darren Finch <finchdarren25@gmail.com>
2022-06-21 21:10:34 -0500

    binary search

--- main.js
+++ main.js
@@ -82,3 +82,31 @@ function montyHall(correctDoorNumber, participantGuesses) {
 	
 	return Math.ceil((wins / participantGuesses.length) * 100)
   }
+
+
+
+// Binary search off the top
+  /**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let l = 0, r = nums.length - 1
+    
+    while (l <= r) {
+        const mid = Math.floor((l + r) / 2)
+        
+        if (nums[mid] == target) {
+            return mid
+        }
+        
+        if (target < nums[mid]) {
+            r = mid - 1
+        } else {
+            l = mid + 1
+        }
+    }
+    
+    return -1
+};
