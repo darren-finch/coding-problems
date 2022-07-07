16f34f52f8b04217771bdf3cdade5a7477cb78ee
Darren Finch <finchdarren25@gmail.com>
2022-07-06 22:38:52 -0500

    WIP count odds

--- main.py
+++ main.py
@@ -179,3 +179,19 @@ class Solution(object):
             return 1
         if len(coins) == 0:
             return 0
+
+
+
+class Solution(object):
+    def countOdds(self, low, high):
+        """
+        :type low: int
+        :type high: int
+        :rtype: int
+        """
+        if low % 2 == 0 and high % 2 == 0:
+            return ((high - low + 1) / 2) - 1
+        elif low % 2 != 0 and high % 2 != 0:
+            return ((high - low + 1) / 2) + 1
+        
+        return ((high - low + 1) / 2)
