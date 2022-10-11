248d952caf4f584694b2be43215efd6ed21f0ee7
Darren Finch <finchdarren25@gmail.com>
2022-10-10 21:38:39 -0500

    WIP: searchInsert algorithm

--- main.js
+++ main.js
@@ -566,3 +566,27 @@ function filterString(value) {
 function sundayHelloWorld() {
   console.log("Hello Sunday, and hello world!")
 }
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ function searchInsert(nums, target) {
+  let left = 0
+  let right = nums.length - 1
+  
+  while (left <= right) {
+      mid = (left + right) / 2
+
+      if (target < nums[mid]) {
+          right = mid - 1
+      } else if (nums[mid] < target) {
+          left = mid + 1
+      } else if (nums[mid] == target) {
+          return mid
+      }
+  }
+
+  return left
+};
