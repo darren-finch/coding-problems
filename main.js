ebe3f9050af21c1f098d3f112c6c7be86541aad0
Darren Finch <finchdarren25@gmail.com>
2022-10-14 21:24:11 -0500

    prefixesDivBy5 WIP 3

--- main.js
+++ main.js
@@ -730,3 +730,27 @@ function factorial(n) {
 
   return result
 };
+
+/**
+ * @param {number[]} nums
+ * @return {boolean[]}
+ */
+ var prefixesDivBy5 = function(nums) {
+  let result = []
+  let actualNum = 0
+  
+  for (let i = nums.length - 1; i >= 0; i--) {
+      if (nums[i] == 1) {
+          actualNum += Math.pow(2, nums.length - i - 1)
+      }
+  }
+
+  for (let j = nums.length - 1; j >= 0; j--) {
+      console.log("Actual num base-10: " + actualNum)
+      console.log("Actual num base-2: " + actualNum.toString(2) + "\n")
+      result[j] = (actualNum % 5 == 0)
+      actualNum = Math.trunc(actualNum / 2)
+  }
+
+  return result
+};
