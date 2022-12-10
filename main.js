b376701856d2eeed6cc3bb0e3d0fe56b97ef5c4b
Darren Finch <finchdarren25@gmail.com>
2022-12-10 09:07:09 -0600

    WIP: maxProduct

--- main.js
+++ main.js
@@ -1942,3 +1942,19 @@ var maxAncestorDiff = function (root) {
 		while (0 < innerQueue.length) {}
 	}
 }
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxProduct = function (root) {
+	// WIP: maxProduct
+}
