a7bef3de9237edfed803fc7c4b96ca51589eed3a
Darren Finch <finchdarren25@gmail.com>
2023-01-30 22:03:20 -0600

    WIP: cloneGraph

--- main.py
+++ main.py
@@ -776,3 +776,18 @@ class Solution:
             #   Add current merged pair to output
             # Return output
             pass
+
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        # Probably use bFS
+        pass
