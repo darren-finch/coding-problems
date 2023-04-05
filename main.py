84e3085ad92b638a342f1d13953a78ffb4b13fd3
Darren Finch <finchdarren25@gmail.com>
2023-04-04 22:43:00 -0500

    WIP: groupAnagrams

--- main.py
+++ main.py
@@ -1260,3 +1260,44 @@ class Solution:
             end += 1
 
         return kbeauty
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # Maps combinations made from
+        letterFrequencyCombinationToBucketIndexMap = {}
+        output = []
+        for string in strs:
+            letterFrequencyMap = {
+                "a": 0,
+                "b": 0,
+                "c": 0,
+                "d": 0,
+                "e": 0,
+                "f": 0,
+                "g": 0,
+                "h": 0,
+                "i": 0,
+                "j": 0,
+                "k": 0,
+                "l": 0,
+                "m": 0,
+                "n": 0,
+                "0": 0,
+                "p": 0,
+                "q": 0,
+                "r": 0,
+                "s": 0,
+                "t": 0,
+                "u": 0,
+                "v": 0,
+                "w": 0,
+                "x": 0,
+                "y": 0,
+                "z": 0,
+            }
+
+            for character in string:
+                letterFrequencyMap[character] += 1
+
+        if
