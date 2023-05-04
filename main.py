5c7b340325e7c4574af89a7c2811a06bb4a7be3c
Darren Finch <finchdarren25@gmail.com>
2023-05-04 09:23:45 -0500

    Ans for Contiguous Subarray Sum (from NeetCode)

--- main.py
+++ main.py
@@ -1958,3 +1958,20 @@ class Solution:
             p1 += 1
 
         return nums1
+
+
+# CHECKED ANSWER FOR THIS ONE
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        rem = {0: -1}
+        total = 0
+
+        for i, n in enumerate(nums):
+            total += n
+            r = total % k
+            if r not in rem:
+                rem[r] = i
+            elif i - rem[r] > 1:
+                return True
+
+        return False
