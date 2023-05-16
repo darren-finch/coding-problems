fd645a341fb664d14ea02ea7620213d6c571561e
Darren Finch <finchdarren25@gmail.com>
2023-05-16 07:04:20 -0500

    Sorting solution to k-closest points

--- main.py
+++ main.py
@@ -2213,3 +2213,10 @@ class Solution:
                 ans.append(num)
 
         return ans
+
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        points.sort(key=lambda point: pow(point[0], 2) + pow(point[1], 2))
+
+        return points[0:k]
