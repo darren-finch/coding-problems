ebab44c2f0f3fd5bbbb5a40bbe3c0ac7084bd1d6
Darren Finch <finchdarren25@gmail.com>
2023-05-18 08:46:20 -0500

    Kth Largest Element in an Array quick select solution from NeetCode

--- main.py
+++ main.py
@@ -2339,3 +2339,25 @@ class Solution:
                 node.random = arrayOfNewNodes[indexOfNewRandomNode]
 
         return arrayOfNewNodes[0]
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        def quickSelect(l: int, r: int) -> int:
+            pivot, p = nums[r], l
+
+            for i in range(l, r):
+                if nums[i] <= pivot:
+                    nums[i], nums[p] = nums[p], nums[i]
+                    p += 1
+
+            nums[p], nums[r] = nums[r], nums[p]
+
+            if len(nums) - k < p:
+                return quickSelect(l, p - 1)
+            elif len(nums) - k > p:
+                return quickSelect(p + 1, r)
+            else:
+                return nums[p]
+
+        return quickSelect(0, len(nums) - 1)
